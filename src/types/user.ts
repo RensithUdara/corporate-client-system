cbaf4a13933336181df18cc7ccb8ffa9de2fc5de
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-03-25 16:17:31 +0530

    Add User and UserGroup interfaces to define user data structure and permissions

---
+++ src/types/user.ts
@@ -0,0 +1,24 @@
+export interface User {
+    firstName: string;
+    lastName: string;
+    email: string;
+    contactNumber: string;
+    password: string;
+    customerType: 'Corporate' | 'Private';
+    companyName?: string;
+    companyAddress?: string;
+    companyAddressField2?: string;
+    city?: string;
+    stateProvince?: string;
+    postalCode?: string;
+    country?: string;
+    activeStatus?: 0 | 1;
+    lastLogin?: string;
+    createdAt?: string;
+    updatedAt?: string;
+  }
+  
+  export interface UserGroup {
+    name: string;
+    privileges: string[];
+  }
